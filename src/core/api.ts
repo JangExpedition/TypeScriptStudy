65275acd87a2d887d6dcc2eb0596285adf0df035
Jangcccci <jangcccci@gmail.com>
2023-11-27 19:57:43 +0900

    비동기 함수로 변경

---
+++ src/core/api.ts
@@ -0,0 +1,28 @@
+import { NewsFeed, NewsDetail } from "../types";
+
+export class Api {
+  xhr: XMLHttpRequest;
+  url: string;
+
+  constructor(url: string) {
+    this.xhr = new XMLHttpRequest();
+    this.url = url;
+  }
+
+  protected async request<AjaxResponse>(): Promise<AjaxResponse> {
+    const response = await fetch(this.url);
+    return (await response.json()) as AjaxResponse;
+  }
+}
+
+export class NewsFeedApi extends Api {
+  async getData(): Promise<NewsFeed[]> {
+    return this.request<NewsFeed[]>();
+  }
+}
+
+export class NewsDetailApi extends Api {
+  async getData(): Promise<NewsDetail> {
+    return this.request<NewsDetail>();
+  }
+}
